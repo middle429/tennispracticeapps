1e931aba4d6febc91df95e18ad19c394228cf754
middle429 <cmon.boo@gmail.com>
2026-05-01 16:30:29 +0900

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/tennis-app",
+  assetPrefix: "/tennis-app",
+};
+
+export default nextConfig;
